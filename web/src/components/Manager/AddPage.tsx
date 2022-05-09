3d8c552f3892e602592a47e59c690ee4f668710f
Emin Umut Gerçek <umutgercek1999@gmail.com>
2022-05-09 18:25:52 +0300

    feat(fe): Improve style of add page

--- web/src/components/Manager/AddPage.tsx
+++ web/src/components/Manager/AddPage.tsx
@@ -1,5 +1,6 @@
 import { Box, Button, Grid, Stack } from "@mui/material";
 import { Link } from "react-router-dom";
+import ButtonCard from "./ButtonCard";
 
 export default function AddPage() {
   // TODO Add emojis for links
@@ -10,16 +11,10 @@ export default function AddPage() {
       sx={{ height: "100vh", justifyContent: "center", alignItems: "center" }}
     >
       <Box textAlign="center">
-        <Stack spacing={5}>
-          <Button size="large" style={{ textTransform: "none" }}>
-            <Link to="/add/animator"> Add Animator</Link>
-          </Button>
-          <Button size="large" style={{ textTransform: "none" }}>
-            <Link to="/add/equipment-person">Add Equipment Person </Link>
-          </Button>
-          <Button size="large" style={{ textTransform: "none" }}>
-            <Link to="/add/customer"> Add Customer</Link>
-          </Button>
+        <Stack spacing={5} direction={"row"}>
+          <ButtonCard name="Animator" to="/add/animator/" />
+          <ButtonCard name="Equipment Person" to="/add/equipment-person/" />
+          <ButtonCard name="Customer" to="/add/customer" />
         </Stack>
       </Box>
     </Grid>
---
+++ web/src/components/Manager/ButtonCard.tsx
@@ -0,0 +1,33 @@
+import {
+  Card,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+  Link,
+} from "@mui/material";
+
+import "./Add.css";
+
+export default function ButtonCard({ name, to }: { name: string; to: string }) {
+  console.log(name, to);
+  return (
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Add
+        </Typography>
+        <Typography variant="h5" component="div"></Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          {name}
+        </Typography>
+        <Typography variant="body2"></Typography>
+      </CardContent>
+      <CardActions className="action">
+        <Button size="large" style={{ textTransform: "none" }}>
+          <Link href={to}>Add</Link>
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
